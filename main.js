12811e6b6664d3298bbcdd5074c6057f5fcb712d
AlexMato <matontialessandro0@gmail.com>
2020-03-19 16:59:25 +0100

    problema sullo sconto

--- main.js
+++ main.js
@@ -13,8 +13,28 @@ var euroCadKm = 0.21;
 //calcolare il prezzo base del biglietto
 var prezzoBase = chilometri * euroCadKm;
 
+//sconto del 40% calcolato
+var prezzoMaggiori
+
+//sconto calcolato al 20%
+var prezzoMinori
+
+//nessuno sconto
+var prezzoOver
+
 console.log("età acquirente = " + etaAcquirente);
 console.log("chilometri da percorrere = " + chilometri);
 console.log("costo base della tratta esente sconti = " + prezzoBase + " euro");
 
-/*applicare lo sconto in base alla fascia di età,20% <18 e 40% >65*/
+// /*applicare lo sconto in base alla fascia di età,20% <18 e 40% >65*/
+if (etaAcquirente < 17) {
+  prezzoMinori = (prezzoBase - (prezzoBase * 20 / 100));
+} else if (etaAcquirente > 18){
+  prezzoMaggiori = (prezzoBase - (prezzoBase * 40 / 100));
+} else (etaAcquirente > 65){
+  prezzoOver = prezzoBase ;
+}
+
+console.log("il costo del biglietto per i minorenni è di " + prezzoMinori);
+console.log("il costo del biglietto per i maggiorenni è di " + prezzoMaggiori);
+console.log("il costo del biglietto per gli over 65 è di " + prezzoOver);
